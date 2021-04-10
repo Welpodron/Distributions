96404ef2485d6fc07648c85654f68519ef6518cc
Welpodron <welpodron@gmail.com>
2021-04-10 12:36:32 +0300

    Updates

--- uniform.js
+++ uniform.js
@@ -33,31 +33,31 @@ var simple = {
 };
 
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-  width = 1160 - margin.left - margin.right,
-  height = 700 - margin.top - margin.bottom;
+var margin = { top: 25, right: 25, bottom: 25, left: 25 },
+  width = 700 - margin.left - margin.right,
+  height = 350 - margin.top - margin.bottom;
 
 var svg = d3
   .select("#chart_1")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("viewBox", `0 0 700 350`)
+  .attr("preserveAspectRatio", "xMidYMid meet")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var svg2 = d3
   .select("#chart_2")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("viewBox", `0 0 700 350`)
+  .attr("preserveAspectRatio", "xMidYMid meet")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var svg3 = d3
   .select("#chart_3")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("viewBox", `0 0 700 350`)
+  .attr("preserveAspectRatio", "xMidYMid meet")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
